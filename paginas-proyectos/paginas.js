8dac16fdad8b6d60584a12927a2e7d2de8579f7f
josuegamonal <69472426+josuegamonal@users.noreply.github.com>
2023-07-03 19:59:43 +0200

    structure ready

--- paginas-proyectos/paginas.js
+++ paginas-proyectos/paginas.js
@@ -5,3 +5,12 @@ linkVolver.addEventListener('click', function() {
   // Redireccionar a la página deseada
   window.location.href = '../index.html';
 });
+
+
+const linkVolver1 = document.querySelector('.volver1');
+
+// Agregar un event listener para responder al clic
+linkVolver1.addEventListener('click', function() {
+  // Redireccionar a la página deseada
+  window.location.href = '../index.html';
+});
